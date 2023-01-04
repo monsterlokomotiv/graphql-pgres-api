97922782eeccc53cad50ceebf65eeb38d388dab1
monsterlokomotiv <Pshycoone92@hotmail.com>
2023-01-04 10:37:34 +0100

    Learned how to use DI for our own services, pretty neat.

--- bookmanagementapi/bookmanagementapi/Query.cs
+++ bookmanagementapi/bookmanagementapi/Query.cs
@@ -1,18 +1,11 @@
 ﻿using BookManagement.Application.Abstractions;
 using BookManagement.Domain;
-using BookManagement.Infrastructure.InMemory;
 
 namespace BookManagement;
 
 public class Query
 {
-    private IBookRepository _bookRepository;
-    public Query()
-    {
-        _bookRepository = new BookRepository();
-    }
+    public Book[] GetBooks([Service] IBookRepository bookRepo) => bookRepo.GetAll().ToArray();
 
-    public Book[] GetBooks() => _bookRepository.GetAll().ToArray();
-
-    public Book? GetBook(int id) => _bookRepository.Get(id);
+    public Book? GetBook([Service] IBookRepository bookRepo, int id) => bookRepo.Get(id);
 }
